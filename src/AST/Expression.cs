160145558e784a3c65b47331809fd376e77e2eab
Øystein Krog <oystein.krog@gmail.com>
2014-02-24 23:21:50 +0100

    More work on Expression type (rename BuiltinTypeExpression to PrimitiveTypeExpression and add support for different integer types and string)

--- src/AST/Expression.cs
+++ src/AST/Expression.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Globalization;
 
 namespace CppSharp.AST
 {
@@ -9,31 +10,146 @@ namespace CppSharp.AST
         public abstract TV Visit<TV>(IExpressionVisitor<TV> visitor);
     }
 
-    public class BuiltinTypeExpression : Expression
+    public class PrimitiveTypeExpression : Expression
     {
-        public long Value { get; set; }
+        private object _parsedValue;
 
-        public BuiltinType Type { get; set; }
-
-        public bool IsHexadecimal
+        public PrimitiveTypeExpression(string expression, object parsedValue)
         {
-            get
+            _parsedValue = parsedValue;
+            DebugText = expression;
+        }
+
+        public static PrimitiveTypeExpression TryCreate(string expression)
+        {
+            PrimitiveType type;
+            object parsedValue;
+            if(TryParseExpression(expression, out type, out parsedValue))
             {
-                if (DebugText == null)
+                return new PrimitiveTypeExpression(expression, parsedValue)
                 {
+                    Type = type
+                };
+            }
+            return null;
+        }
+
+        public PrimitiveType Type { get; private set; }
+
+        private static bool TryParseExpression(string expression, out PrimitiveType primitiveType, out object parsedValue)
+        {
+            long valueAsInteger;
+            if (ParseAsLong(expression, out valueAsInteger))
+            {
+                parsedValue = valueAsInteger;
+                if (valueAsInteger <= System.SByte.MaxValue && valueAsInteger > System.SByte.MinValue)
+                {
+                    primitiveType = PrimitiveType.Int8;
+                }
+                else if (valueAsInteger <= System.Byte.MaxValue && valueAsInteger > System.Byte.MinValue)
+                {
+                    primitiveType = PrimitiveType.UInt8;
+                }
+                else if (valueAsInteger <= System.Int16.MaxValue && valueAsInteger > System.Int16.MinValue)
+                {
+                    primitiveType = PrimitiveType.Int16;
+                }
+                else if (valueAsInteger <= System.UInt16.MaxValue && valueAsInteger > System.UInt16.MinValue)
+                {
+                    primitiveType = PrimitiveType.UInt16;
+                }
+                else if (valueAsInteger <= System.Int32.MaxValue && valueAsInteger > System.Int32.MinValue)
+                {
+                    primitiveType = PrimitiveType.Int32;
+                }
+                else if (valueAsInteger <= System.UInt32.MaxValue && valueAsInteger > System.UInt32.MinValue)
+                {
+                    primitiveType = PrimitiveType.UInt32;
+                }
+                else if (valueAsInteger <= System.Int64.MaxValue && valueAsInteger > System.Int64.MinValue)
+                {
+                    primitiveType = PrimitiveType.Int64;
+                }
+//                else if (valueAsInteger <= System.UInt64.MaxValue && valueAsInteger < System.UInt64.MinValue)
+//                {
+//                    primitiveType = PrimitiveType.UInt64;
+//                }
+                else
+                {
+                    primitiveType = PrimitiveType.Void;
                     return false;
                 }
-                return DebugText.Contains("0x") || DebugText.Contains("0X");
+
+                return true;
             }
+            string parsedString;
+            if (ParseAsString(expression, out parsedString))
+            {
+                primitiveType = PrimitiveType.WideChar;
+                parsedValue = parsedString;
+                return true;
+            }
+            primitiveType = PrimitiveType.Void;
+            parsedValue = null;
+            return false;
+        }
+
+        private static bool ParseAsString(string expression, out string valueAsString)
+        {
+            if (expression.StartsWith("\"") && expression.EndsWith("\""))
+            {
+                valueAsString = expression.TrimStart('"').TrimEnd('"');
+                return true;
+            }
+            valueAsString = null;
+            return false;
+        }
+
+        private static bool ParseAsLong(string num, out long val)
+        {
+            if (num.StartsWith("0x", StringComparison.CurrentCultureIgnoreCase))
+            {
+                num = num.Substring(2);
+
+                return long.TryParse(num, NumberStyles.HexNumber,
+                    CultureInfo.CurrentCulture, out val);
+            }
+
+            return long.TryParse(num, out val);
+        }
+
+        private static bool IsHexadecimal(string val)
+        {
+            if (val == null)
+            {
+                return false;
+            }
+            return val.Contains("0x") || val.Contains("0X");
         }
 
         public override string ToString()
         {
-            var printAsHex = IsHexadecimal && Type.IsUnsigned;
-            var format = printAsHex ? "x" : string.Empty;
-            var value = Type.IsUnsigned ? Value.ToString(format) :
-                ((long)Value).ToString(format);
-            return printAsHex ? "0x" + value : value;
+            switch (Type)
+            {
+                case PrimitiveType.WideChar:
+                    return '"' + (string) _parsedValue + '"';
+                case PrimitiveType.Int8:
+                case PrimitiveType.UInt8:
+                case PrimitiveType.Int16:
+                case PrimitiveType.UInt16:
+                case PrimitiveType.Int32:
+                case PrimitiveType.UInt32:
+                case PrimitiveType.Int64:
+                case PrimitiveType.UInt64:
+                {
+                    bool printAsHex = IsHexadecimal(DebugText);
+                    string format = printAsHex ? "x" : string.Empty;
+                    return ((long) _parsedValue).ToString(format);
+                }
+                default:
+                    throw new ArgumentOutOfRangeException();
+            }
+           
         }
 
         public override T Visit<T>(IExpressionVisitor<T> visitor)
@@ -44,6 +160,6 @@ namespace CppSharp.AST
 
     public interface IExpressionVisitor<out T>
     {
-        T VisitBuiltinExpression(BuiltinTypeExpression builtinType);
+        T VisitBuiltinExpression(PrimitiveTypeExpression primitiveType);
     }
 }
