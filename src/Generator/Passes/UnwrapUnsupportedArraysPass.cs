0b1d314cc91fae87a961e0b4bb26c3e012b8b4d1
Øystein Krog <oystein.krog@gmail.com>
2014-02-28 13:14:08 +0100

    Cleanup work in UnwrapUnsupportedArraysPass

--- src/Generator/Passes/UnwrapUnsupportedArraysPass.cs
+++ src/Generator/Passes/UnwrapUnsupportedArraysPass.cs
@@ -7,7 +7,7 @@ using Type = CppSharp.AST.Type;
 namespace CppSharp.Passes
 {
     /// <summary>
-    ///     Some kinds of fields are not supported in c#, in particular fixed arrays can only be of a primitive type.
+    ///     Some kinds of fields are not supported in C# pinvoke, in particular fixed arrays can only be of a primitive type.
     /// </summary>
     public class UnwrapUnsupportedArraysPass : TranslationUnitPass
     {
@@ -39,88 +39,17 @@ namespace CppSharp.Passes
 
         private IEnumerable<Field> TryUnwrapField(Class @class, Field field)
         {
-            // check if field is an array
+            // check that field is a constant size array
             var arrayType = field.Type as ArrayType;
             if (arrayType != null && arrayType.SizeType == ArrayType.ArraySize.Constant)
             {
                 Type typeInArray = arrayType.Type.Desugar();
-                long unwrapCount = 0;
-                Type unwrapType = null;
-                // in bits
-                long unwrapTypeWidth = 0;
 
-                if (typeInArray.IsPrimitiveType())
-                {
-                    // no need to unwrap, as we can handle generate fixed arrays of primitive types
-                    yield break;
-                }
+                if (IsSupportedArray(typeInArray)) yield break;
 
-                if (typeInArray.IsPointerToPrimitiveType())
-                {
-                    // need to unwrap if field is array of pointer to primitive type
-                    // int* A[N] 
-                    // becomes
-                    // int* A_0;
-                    // int* A_N;
-                    // unwrap to multiple pointer fields
-                    unwrapType = new PointerType
-                    {
-                        QualifiedPointee = ((PointerType) typeInArray).QualifiedPointee
-                    };
-                    unwrapCount = arrayType.Size;
-                    // TODO: get type width from driver TargetInfo!
-                    unwrapTypeWidth = 0;
-                }
-                else if (typeInArray.IsPointerToArrayType())
-                {
-                    // need to unwrap if field is array of pointer to array
-                    // A (*int[N])[M]
-                    // becomes
-                    // int** X_0;
-                    // int** X_M;
-                    var innerArray = (ArrayType) ((PointerType) typeInArray).Pointee;
-
-                    unwrapType = new PointerType
-                    {
-                        QualifiedPointee = new QualifiedType(new PointerType
-                        {
-                            QualifiedPointee = new QualifiedType(innerArray.Type)
-                        })
-                    };
-                    unwrapCount = arrayType.Size;
-                    unwrapTypeWidth = Driver.TargetInfo.PointerWidth;
-                }
-                else if (typeInArray is ArrayType)
-                {
-                    // need to unwrap if field is array of array
-                    // int A[N][M]
-                    // becomes
-                    // int A_0[M];
-                    // int A_N[M];
-                    Type innerArray = ((ArrayType) (typeInArray)).Type;
-                    unwrapType = new ArrayType
-                    {
-                        Size = ((ArrayType) (typeInArray)).Size,
-                        SizeType = ArrayType.ArraySize.Constant,
-                        Type = innerArray
-                    };
-                    unwrapCount = arrayType.Size / ((ArrayType)(typeInArray)).Size;
-                    // TODO: get type width from driver TargetInfo!
-                    unwrapTypeWidth = 0;
-                }
-                else if (!typeInArray.IsPrimitiveType())
-                {
-                    // need tp unwrap if field is array of complex type
-                    // Struct A[N]
-                    // becomes
-                    // Struct A_0;
-                    // Struct A_N;
-                    unwrapType = typeInArray;
-                    unwrapCount = arrayType.Size;
-                    // TODO: get type width from driver TargetInfo!
-                    unwrapTypeWidth = 0;
-                }
-                else
+                var unwrapInfo = GetUnwrapInfo(arrayType, typeInArray);
+                
+                if(unwrapInfo == null)
                 {
                     Driver.Diagnostics.EmitWarning("Failed to unwrap {0}.{1} ({2})",
                         @class.Name, field.Name, field.Type);
@@ -130,19 +59,20 @@ namespace CppSharp.Passes
                 Driver.Diagnostics.EmitMessage("Unwrapping {0}.{1} ({2})",
                     @class.Name, field.Name, field.Type);
 
-                for (int i = 0; i < unwrapCount; i++)
+                for (int i = 0; i < unwrapInfo.Value.UnwrapCount; i++)
                 {
+                    // field[N] becomes field_0 .. field_N
                     string unwrappedName = field.Name + "_" + i;
 
                     var unwrappedField = new Field
                     {
                         Name = unwrappedName,
-                        Offset = (uint) (field.Offset + i*unwrapTypeWidth),
+                        Offset = (uint) (field.Offset + i*unwrapInfo.Value.UnwrapTypeWidth),
                         Class = @class,
                         DebugText = field.DebugText,
                         DefinitionOrder = field.DefinitionOrder,
                         Namespace = field.Namespace,
-                        QualifiedType = new QualifiedType(unwrapType, field.QualifiedType.Qualifiers),
+                        QualifiedType = new QualifiedType(unwrapInfo.Value.UnwrapType, field.QualifiedType.Qualifiers),
                         IgnoreFlags = field.IgnoreFlags,
                         IsGenerated = field.IsGenerated,
                         ExcludeFromPasses = field.ExcludeFromPasses,
@@ -169,5 +99,107 @@ namespace CppSharp.Passes
                 }
             }
         }
+
+        private static bool IsSupportedArray(Type typeInArray)
+        {
+            if (typeInArray.IsPrimitiveType())
+            {
+                // no need to unwrap, as it is legal to have fixed arrays of primitive types (in C# pinvoke)
+                return true;
+            }
+            return false;
+        }
+
+        private UnwrapInfo? GetUnwrapInfo(ArrayType arrayType, Type typeInArray)
+        {
+            long unwrapCount;
+            Type unwrapType;
+            // in bits
+            long unwrapTypeWidth;
+            if (typeInArray.IsPointerToPrimitiveType())
+            {
+                // need to unwrap if field is array of pointer to primitive type
+                // int* A[N] 
+                // becomes
+                // int* A_0;
+                // int* A_N;
+                // unwrap to multiple pointer fields
+                unwrapType = new PointerType
+                {
+                    QualifiedPointee = ((PointerType)typeInArray).QualifiedPointee
+                };
+                unwrapCount = arrayType.Size;
+                // TODO: get type width from driver TargetInfo!
+                unwrapTypeWidth = 0;
+            }
+            else if (typeInArray.IsPointerToArrayType())
+            {
+                // need to unwrap if field is array of pointer to array
+                // A (*int[N])[M]
+                // becomes
+                // int** X_0;
+                // int** X_M;
+                var innerArray = (ArrayType)((PointerType)typeInArray).Pointee;
+
+                unwrapType = new PointerType
+                {
+                    QualifiedPointee = new QualifiedType(new PointerType
+                    {
+                        QualifiedPointee = new QualifiedType(innerArray.Type)
+                    })
+                };
+                unwrapCount = arrayType.Size;
+                unwrapTypeWidth = Driver.TargetInfo.PointerWidth;
+            }
+            else if (typeInArray is ArrayType)
+            {
+                // need to unwrap if field is array of array
+                // int A[N][M]
+                // becomes
+                // int A_0[M];
+                // int A_N[M];
+                Type innerArray = ((ArrayType)(typeInArray)).Type;
+                unwrapType = new ArrayType
+                {
+                    Size = ((ArrayType)(typeInArray)).Size,
+                    SizeType = ArrayType.ArraySize.Constant,
+                    Type = innerArray
+                };
+                unwrapCount = arrayType.Size / ((ArrayType)(typeInArray)).Size;
+                // TODO: get type width from driver TargetInfo!
+                unwrapTypeWidth = 0;
+            }
+            else if (!typeInArray.IsPrimitiveType())
+            {
+                // need tp unwrap if field is array of complex type
+                // Struct A[N]
+                // becomes
+                // Struct A_0;
+                // Struct A_N;
+                unwrapType = typeInArray;
+                unwrapCount = arrayType.Size;
+                // TODO: get type width from driver TargetInfo!
+                unwrapTypeWidth = 0;
+            }
+            else
+            {
+                return null;
+            }
+
+            return new UnwrapInfo
+            {
+                UnwrapType = unwrapType,
+                UnwrapCount = unwrapCount,
+                UnwrapTypeWidth = unwrapTypeWidth
+            };
+        }
+    }
+
+    internal struct UnwrapInfo
+    {
+        public Type UnwrapType { get; set; }
+        // in bits!
+        public long UnwrapTypeWidth { get; set; }
+        public long UnwrapCount { get; set; }
     }
 }
