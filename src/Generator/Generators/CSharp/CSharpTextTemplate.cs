7239cd0052f381d70628593047be84c44ae7da94
Øystein Krog <oystein.krog@gmail.com>
2014-02-28 13:03:17 +0100

    [HAX] Modify CSharpTextTemplate to print CharSet and ExactSpelling attributes for internal functions

--- src/Generator/Generators/CSharp/CSharpTextTemplate.cs
+++ src/Generator/Generators/CSharp/CSharpTextTemplate.cs
@@ -2748,6 +2748,8 @@ namespace CppSharp.Generators.CSharp
             WriteLine("CallingConvention = global::System.Runtime.InteropServices.CallingConvention.{0},",
                 callConv);
 
+            WriteLineIndent("CharSet = CharSet.Ansi, ExactSpelling = true,");
+
             WriteLineIndent("EntryPoint=\"{0}\")]", function.Mangled);
 
             if (function.ReturnType.Type.Desugar().IsPrimitiveType(PrimitiveType.Bool))
